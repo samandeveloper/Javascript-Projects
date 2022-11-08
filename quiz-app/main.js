b066c97e2203081a50ed09683f73fbeac95d0d1a
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2022-11-07 22:24:23 -0700

    Update main.js

--- quiz-app/main.js
+++ quiz-app/main.js
@@ -46,18 +46,14 @@ let quizData = [
     }
 ];
 
-
-
 //keep track of current question starts with 0
 let currentQuiz = 0;
 //for counting the score
 let score = 0;
-
 let quiz = document.getElementById("quiz");
 
 //the question
 let questionE1 = document.getElementById("question");
-
 
 //.answer because it's a class of each input--radio buttons
 let answerEls = document.querySelectorAll('.answer');
@@ -84,78 +80,50 @@ function loadQuiz() {
     b_text.innerHTML = currentQuizData.b;
     c_text.innerHTML = currentQuizData.c;
     d_text.innerHTML = currentQuizData.d;
-
-    // //go to the next question
-    // currentQuiz++;
 }
 
 //if we have an answer--select and deselect--store the selected when we click submit
 function getSelected() {
-
     let answer = undefined;
     //use forEach to call the function in an array for each element
-    //we write answerEl from ourselfs
     answerEls.forEach((answerEl) => {
-        // console.log(answer.checked)
         //if any of the four answers checked
         if (answerEl.checked) {
             //id can be: a b c d
             answer = answerEl.id;
-            console.log(answer);
         }
     });
-
     return answer;
-
 }
 
-//if we select an answer then submit button the answer from the previous question is still shown--with deselectAnswer() we prevent that 
+//if we select an answer then submit button the answer from the previous question is still shown--with deselectAnswer() we prevent this
 function deselectAnswer() {
-
     answerEls.forEach((answerEl) => {
         answerEl.checked = false;
     });
 }
 
-
-
 //for going to the next question--click the submitBtn
 submitBtn.addEventListener("click", () => {
     //check to see the answer
     let answer = getSelected();
-
-
+    
     //if we have an answer for a question it should go to the next question
     if (answer) {
-
         //if we have a currect answer add to the score
         if (answer === quizData[currentQuiz].correct) {
             score++;
         }
 
-        //go to the next question--currentQuize is a question that we are in
+        //go to the next question--currentQuize is a question that we are already in
         currentQuiz++;
 
         //quizeData length is 3--go to the next question until the questions finished
         if (currentQuiz < quizData.length) {
             loadQuiz();
         } else {
-            //show results
-
-            // alert('You finished');
             quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions</h2>`
         }
     }
 
-
-
-    // //quizeData length is 3
-    // if (currentQuiz < quizData.length) {
-    //     loadQuiz();
-    // } else {
-    //     //show results
-
-    //     alert('You finished');
-    // }
 });
-
