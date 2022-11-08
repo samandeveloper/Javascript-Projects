51b5af79faa4972e5edde40c61d8daf611c0c812
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2022-11-07 22:29:12 -0700

    Update index.js

--- color-flipper/index.js
+++ color-flipper/index.js
@@ -1,5 +1,4 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-
 document.getElementById("btn").addEventListener("click", changeColor)  //cal the function in addEventListener
 
 function changeColor(){
@@ -10,9 +9,6 @@ function changeColor(){
     document.body.style.backgroundColor =colors[pickRandomColor]   //we must use the "document" to make it work
 }
 
-
-
-//way2--to have more colors
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");   //return the first element that matches--or .getElementsByClassName
@@ -22,8 +18,6 @@ btn.addEventListener("click", function () {
   for (let i = 0; i < 6; i++) {     //because hex numbers are 6 digits
     hexColor += hex[getRandomNumber()];
   }
-  // console.log(hexColor);
-
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
